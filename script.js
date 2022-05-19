f10f1a31f63aeb94be170ec40523961207b25fe9
Nathan Vance <Natevance01@gmail.com>
2022-05-19 09:03:15 -0500

    created html boiler plate, style sheet, created game board and tile scripts

---
+++ script.js
@@ -0,0 +1,10 @@
+import Grid from "./grid.js"
+import Tile from "./Tile.js"
+
+const gameBoard = document.getElementById("game-board")
+
+const grid = new Grid(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+
+
